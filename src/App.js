7ba1e5a4c58ca08b777c928ef7ba930d0291970e
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-19 03:16:32 -0700

    ✔ Done fetching API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+   const [loading, setLoading] = useState(false);
+
+   // fetch api
+   const fetchImage = async () => {
+      setLoading(true);
+      let url;
+      // setting url for different usecase
+      url = `${mainUrl}?client_id=MXVGpnGNqwNFOPTNmscbJYdvGlq5pbwUDoSPA2Er0Qo `;
+      try {
+        
+         const response = await fetch(url);
+         const data = await response.json();
+         console.log(data);
+      } catch (error) {
+         console.log(error);
+         setLoading(false);
+      }
+   };
+   useEffect(() => {
+      fetchImage();
+    
+   }, []);
+
+   return <h2>stock photos starter</h2>;
 }
 
-export default App
+export default App;
